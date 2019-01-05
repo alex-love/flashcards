07136876243d9525f4f34f95adc82c54b798667b
alex <alex@cornerboothmedia.com>
2019-01-04 16:20:58 -0800

    adding backend

--- tojson.py
+++ tojson.py
@@ -1,4 +1,15 @@
-import json
+wordTuple = []
+with open('words.txt', 'r') as f:
+    for line in f:
+        tmp = line.split(',',1)
+        word = tmp[0]
+        definition = tmp[1]
+        wordTuple.append((word, definition))
 
-def convert_to_json():
+## at this point wordTuple is a tuple with word in [0] and definition in [1]
+for word in wordTuple:
+
+    print(wordTuple[0][0])
+
+""" def convert_to_json(): """
     
